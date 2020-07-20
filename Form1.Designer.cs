f5ba76f292f6cd5dd8ae770cee2f4c59f596bfa3
debby2020 <68416495+debby2020@users.noreply.github.com>
2020-07-20 12:23:24 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace growidchecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.fourletter = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.found = new System.Windows.Forms.Label();
+            this.badattempt = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.error = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "normal search";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Socket initializing.";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // fourletter
+            // 
+            this.fourletter.Interval = 900;
+            this.fourletter.Tick += new System.EventHandler(this.fourletter_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(218, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            14,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(32, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(256, 15);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(85, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Add Number";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(256, 46);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(82, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Write to File";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Found:";
+            // 
+            // found
+            // 
+            this.found.AutoSize = true;
+            this.found.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.found.ForeColor = System.Drawing.Color.Green;
+            this.found.Location = new System.Drawing.Point(55, 76);
+            this.found.Name = "found";
+            this.found.Size = new System.Drawing.Size(15, 18);
+            this.found.TabIndex = 7;
+            this.found.Text = "0";
+            // 
+            // badattempt
+            // 
+            this.badattempt.AutoSize = true;
+            this.badattempt.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.badattempt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.badattempt.Location = new System.Drawing.Point(182, 78);
+            this.badattempt.Name = "badattempt";
+            this.badattempt.Size = new System.Drawing.Size(15, 18);
+            this.badattempt.TabIndex = 9;
+            this.badattempt.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(153, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Bad:";
+            // 
+            // error
+            // 
+            this.error.AutoSize = true;
+            this.error.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.error.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.error.Location = new System.Drawing.Point(326, 78);
+            this.error.Name = "error";
+            this.error.Size = new System.Drawing.Size(15, 18);
+            this.error.TabIndex = 11;
+            this.error.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(287, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Error:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(164, 12);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            4500,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown2.TabIndex = 12;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1200,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 104);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.error);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.badattempt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.found);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "GrowID Checker - Unlimited";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer fourletter;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label found;
+        private System.Windows.Forms.Label badattempt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label error;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,245 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace growidchecker
+{
+    public partial class Form1 : Form
+    {
+  
+        public ChromiumWebBrowser browser;
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("www.growtopiagame.com/account");
+            this.Controls.Add(browser);
+            browser.Dock = DockStyle.Left;
+            browser.Visible = true;
+            browser.LoadingStateChanged += OnLoadingStateChangeds;
+        }
+        bool fourlettercheck = false;
+        string currenAcc;
+        string textpath = "";
+        int foundcount = 0;
+        int badatt = 0;
+        int erroratt = 0;
+        private async void OnLoadingStateChangeds(object sender, LoadingStateChangedEventArgs args)
+        {
+            if (!args.IsLoading)
+            {
+                if(fourlettercheck == true)
+                {
+                    string HTMSL = await browser.GetSourceAsync();
+                    if (HTMSL.Contains("Unable to locate an account"))
+                    {
+                        if(checkBox2.Checked)
+                        {
+                            using (StreamWriter w = File.AppendText(textpath))
+                            {
+                                w.WriteLine(currenAcc);
+                            }
+                            foundcount += 1;
+    
+                        }
+                        else
+                        {
+                            MessageBox.Show("available acc: " + currenAcc);
+                            foundcount += 1;
+                            found.Text = foundcount.ToString();
+                        }
+                    }
+                    else if (HTMSL.Contains("That email doesn't match"))
+                    {
+                        badatt += 1;
+  
+                    }
+                    else
+                    {
+                        erroratt += 1;
+                    }
+                }
+                else
+                {
+                    string HTML = await browser.GetSourceAsync();
+                    if (HTML.Contains("Unable to locate an account"))
+                    {
+                        MessageBox.Show("available acc");
+                    }
+                    else if (HTML.Contains("That email doesn't match"))
+                    {
+                        MessageBox.Show("unavailable acc");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ready To Use");
+                    }
+                }
+
+
+            }
+        }
+        public void enable()
+        {
+            button1.Text = "Test";
+            button1.Enabled = true;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            InitBrowser();
+        }
+        Random r = new Random();
+        private string RandomString(int length)
+        {
+            const string pool = "abcdefghijklmnopqrstuvwxyz0123456789"; //0123456789
+            var builder = new StringBuilder();
+
+            for (var i = 0; i < length; i++)
+            {
+                var c = pool[r.Next(0, pool.Length)];
+                builder.Append(c);
+            }
+
+            return builder.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    if(textBox1.TextLength > 3)
+                    {
+                        StringBuilder builder = new StringBuilder();
+                        builder.AppendLine("document.getElementById('name').value=\"" + textBox1.Text + "\"");
+                        builder.AppendLine("document.getElementById('email').value=\"" + RandomString(9) + "@gmail.com" + "\"");
+                        builder.AppendLine("document.forms[0].submit()");
+                        browser.ExecuteScriptAsync(builder.ToString());
+                    }
+                    else
+                    {
+                        MessageBox.Show("username need > 3");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Empty username");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Please wait ready to use message.");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+            MessageBox.Show("please wait ready to use message");
+            enable();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    if (!String.IsNullOrWhiteSpace(textBox1.Text))
+                    {
+                        if (textBox1.TextLength > 3)
+                        {
+                            StringBuilder builder = new StringBuilder();
+                            builder.AppendLine("document.getElementById('name').value=\"" + textBox1.Text + "\"");
+                            builder.AppendLine("document.getElementById('email').value=\"" + RandomString(9) + "@gmail.com" + "\"");
+                            builder.AppendLine("document.forms[0].submit()");
+                            browser.ExecuteScriptAsync(builder.ToString());
+                        }
+                        else
+                        {
+                            MessageBox.Show("username need > 3");
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Empty username");
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Please wait ready to use message.");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fourletter.Interval = Convert.ToInt32(numericUpDown2.Value);
+            fourletter.Start();
+            textBox1.Text = "Checking:";
+            textBox1.Enabled = false;
+        }
+
+        private void fourletter_Tick(object sender, EventArgs e)
+        {
+            StringBuilder builder1 = new StringBuilder();
+            builder1.Append("abcdefghijklmnopqrstuvwxyz");
+            if (checkBox1.Checked)
+            {
+                builder1.Append("0123456789");
+            }
+            var chars = builder1.ToString();//
+            int output = 0;
+            decimal std = numericUpDown1.Value;
+            output += Convert.ToInt32(Convert.ToString(std));
+            var stringChars = new char[output];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("document.getElementById('name').value=\"" + finalString + "\"");
+            builder.AppendLine("document.getElementById('email').value=\"" + RandomString(9) + "@gmail.com" + "\"");
+            builder.AppendLine("document.forms[0].submit()");
+            browser.ExecuteScriptAsync(builder.ToString());
+            fourlettercheck = true;
+            currenAcc = finalString;
+            textBox1.Text = "Checking:"+currenAcc;
+            badattempt.Text = badatt.ToString();
+            found.Text = foundcount.ToString();
+            error.Text = erroratt.ToString();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox2.Checked == true)
+            {
+                OpenFileDialog theDialog = new OpenFileDialog();
+                theDialog.Title = "Open Text File";
+                theDialog.Filter = "TXT files|*.txt";
+                string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                theDialog.InitialDirectory = desktop;
+                if (theDialog.ShowDialog() == DialogResult.OK)
+                {
+                    MessageBox.Show("Selected.");
+                    StreamWriter stream = new StreamWriter(theDialog.FileName);
+                    stream.WriteLine("## debby#2020");
+                    stream.Close();
+                    textpath = theDialog.FileName;
+                }
+                }
+        }
+    }
+}
